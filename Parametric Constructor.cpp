82144f846111044bb6e95055d47f63e6636a8ac4
65NinadA3 <ninadphatak123@gmail.com>
2025-09-03 23:16:03 +0530

    Create Parametric Constructor.cpp

---
+++ Parametric Constructor.cpp
@@ -0,0 +1,24 @@
+//Name : Ninad Phatak
+//PRN : 24070123065 
+//Batch : ENTC A3
+#include <iostream>
+using namespace std;
+class Date {
+    int day, month, year;
+public:
+    // Parametric constructor
+    Date(int d, int m, int y) {
+        day = d;
+        month = m;
+        year = y;
+    }
+    void showDate() {
+        cout << day << "/" << month << "/" << year % 100 << endl;
+    }
+};
+int main() {
+    Date d1(28,8,2025);
+    cout << "Date: ";
+    d1.showDate();
+    return 0;
+}
