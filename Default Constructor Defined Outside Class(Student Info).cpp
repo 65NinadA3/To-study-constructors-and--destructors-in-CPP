24cc2c1f21d1605327097a814c929f480a6a6d7c
65NinadA3 <ninadphatak123@gmail.com>
2025-09-03 23:03:15 +0530

    Create Default Constructor Defined Outside Class(Student Info).cpp

---
+++ Default Constructor Defined Outside Class(Student Info).cpp
@@ -0,0 +1,32 @@
+//Name : Ninad Phatak
+//PRN : 24070123065
+//Batch : ENTC A3
+#include <iostream>
+using namespace std;
+class student {
+public:
+    string name;
+    int rollNumber;
+    char grade;
+    student();
+    void display();
+};
+student::student() {
+    cout<<"Enter Name: ";
+    cin>>name;
+    cout<<"Enter Roll Number: ";
+    cin>>rollNumber;
+    cout<<"Enter Grade: ";
+    cin>>grade;
+}
+void student::display() {
+    cout<<"Student Info"<<endl;
+    cout<<"Name: "<<name<<endl;
+    cout<<"Roll Number:"<<rollNumber<<endl;
+    cout<<"Grade:"<<grade<<endl;
+}
+int main() {
+    student s1;
+    s1.display();  
+    return 0;
+}
